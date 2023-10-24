8666a7f8079a8179762c5b4d45fa1ae37415f334
Diego Rubin <contact@diegorubin.dev>
2023-10-24 08:37:19 -0300

    chore: updating setup.py with long description

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="lifeguard-notification-msteams",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/LifeguardSystem/lifeguard-notification-msteams",
     author="Diego Rubin",
     author_email="contact@diegorubin.dev",
@@ -10,7 +14,16 @@ setup(
     scripts=[],
     include_package_data=True,
     description="Lifeguard integration with MS Teams",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=["lifeguard", "pymsteams"],
-    classifiers=["Development Status :: 3 - Alpha"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Monitoring",
+    ],
     packages=find_packages(),
 )
